6c2c6db4aca3e49d86bb13792cb7773ba4cbc6b8
Mariya Pchelnik <mariyapch@gmail.com>
2021-01-06 14:39:17 +0300

    delete override methods

--- Clean Code/src/main/java/planes/ExperimentalPlane.java
+++ Clean Code/src/main/java/planes/ExperimentalPlane.java
@@ -25,16 +25,6 @@ public class ExperimentalPlane extends Plane{
     }
 
     @Override
-    public boolean equals(Object o) {
-        return super.equals(o);
-    }
-
-    @Override
-    public int hashCode() {
-        return super.hashCode();
-    }
-
-    @Override
     public String toString() {
         return "experimentalPlane{" +
                 "model='" + model + '\'' +
